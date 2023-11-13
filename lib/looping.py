d4b4fab59954c7c3421563c65ff9c8be3eaf71be
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-11-12 22:41:27 -0500

    lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [integ_num**2 for integ_num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        i+=1
+        if (i%3==0 and i%5==0):
+            print('FizzBuzz')
+        elif i%3==0:
+            print('Fizz')
+        elif i%5==0:
+            print('Buzz')
+        else:
+            print(i)
